bae446d460da06d103e09f3214794320e1722b3c
Wande <wanderlustking11@gmail.com>
2024-04-26 12:09:41 -0400

    Add CSS modules

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,15 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    cache: 'no-store', // disables caching (useful for when we have data that changes frequently)
+    // cache: 'no-store', // disables caching (useful for when we have data that changes frequently)
+    next: { revalidate: 10 }, // run background job and get fresh data every 10 seconds
   });
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
